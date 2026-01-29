2fb6a0a68a043ae52dfbf496dcae5d7b0a3c7751
MagnusGul <118620376+MagnusGul@users.noreply.github.com>
2026-01-29 15:04:25 +0300

    Update sidebarItems.js

--- sidebarItems.js
+++ sidebarItems.js
@@ -2,13 +2,12 @@ export const sidebarItems = [
     { type: "juz", number: 1 },
     { type: "surah", number: 1, name: "Аль-Фатиха" },
     { type: "ruku", id: 1, title: "Наилучшая молитва", tags: ["Молитвы"], chapter: 1, from: 1, to: 7, juz: 1 },
-    
+
     { type: "surah", number: 1, name: "Аль-Бакара" },
-    
     {
         type: "ruku",
         id: 2,
-        title: "1. Уверовавшие и Неверующие",
+        title: "1. Богобоязненные и куффары",
         description: [
             {
                 text: "Это руководство для богобоязненных",
@@ -26,18 +25,17 @@ export const sidebarItems = [
         juz: 1,
         boldSeparators: [5]
     },
-    
     {
         type: "ruku",
         id: 3,
-        title: "2. Лицемеры",
+        title: "2. Мунафики",
         description: [
             {
-                text: "Лицемеры и последствия лицемерия",
+                text: "Мунафики и последствия нифак",
                 ref: "2:[8–16]"
             },
             {
-                text: "Притчи о них",
+                text: "Аналогии о них",
                 ref: "2:[17–20]"
             }
         ],
@@ -48,7 +46,6 @@ export const sidebarItems = [
         juz: 1,
         boldSeparators: [16]
     },
-    
     {
         type: "ruku",
         id: 4,
@@ -82,7 +79,6 @@ export const sidebarItems = [
         juz: 1,
         boldSeparators: [22, 24, 25, 27, 29]
     },
-    
     {
         type: "ruku",
         id: 5,
@@ -101,42 +97,20 @@ export const sidebarItems = [
         juz: 1,
         boldSeparators: [30, 33, 35, 36, 37]
     },
-
-    
-    { type: "ruku", id: 6, title: "5 [3:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 7, title: "6 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 8, title: "7 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 9, title: "8 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 10, title: "9 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 11, title: "10 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 12, title: "11 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 13, title: "12 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 14, title: "13 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 15, title: "14 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 16, title: "15 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 17, title: "16 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 18, title: "17 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 19, title: "18 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 20, title: "19 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 21, title: "20 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 22, title: "21 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 23, title: "22 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 24, title: "23 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 25, title: "24 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 26, title: "25 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 27, title: "26 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 28, title: "27 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 29, title: "28 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 30, title: "29 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 31, title: "30 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 32, title: "31 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 33, title: "32 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 34, title: "33 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 35, title: "34 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 36, title: "35 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 37, title: "36 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 38, title: "37 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 39, title: "38 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 40, title: "39 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-    { type: "ruku", id: 41, title: "40 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
-];
+    {
+        type: "ruku",
+        id: 6,
+        title: "5",
+        description: [
+            { text: "Необходимость в откровениях ﷲ для руководства", ref: "2:[38-39]" },
+            { text: "Завет ﷲ с Сынами Израиля", ref: "2:[40–43]" },
+            { text: "Вы наставляете других, но забываете себя?", ref: "2:[44]" },
+            { text: "Помощь Аллаха приходит с терпением и Салятом", ref: "2:[45-46]" },
+        ],
+        tags: [],
+        chapter: 2,
+        from: 38,
+        to: 46,
+        juz: 1,
+        boldSeparators: [39, 43, 44, 46]
+    },
