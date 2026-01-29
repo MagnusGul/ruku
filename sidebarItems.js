d3d5ec5e47a686a7bf63606c1693793781237fa0
MagnusGul <118620376+MagnusGul@users.noreply.github.com>
2026-01-29 14:24:13 +0300

    Create sidebarItems.js

---
+++ sidebarItems.js
@@ -0,0 +1,142 @@
+export const sidebarItems = [
+    { type: "juz", number: 1 },
+    { type: "surah", number: 1, name: "Аль-Фатиха" },
+    { type: "ruku", id: 1, title: "Наилучшая молитва", tags: ["Молитвы"], chapter: 1, from: 1, to: 7, juz: 1 },
+    
+    { type: "surah", number: 1, name: "Аль-Бакара" },
+    
+    {
+        type: "ruku",
+        id: 2,
+        title: "1. Уверовавшие и Неверующие",
+        description: [
+            {
+                text: "Это руководство для богобоязненных",
+                ref: "2:[1–5]"
+            },
+            {
+                text: "Предупреждение бесполезно для тех, кто отвергает веру",
+                ref: "2:[6–7]"
+            }
+        ],
+        tags: ["Неверующие", "Коран"],
+        chapter: 2,
+        from: 1,
+        to: 7,
+        juz: 1,
+        boldSeparators: [5]
+    },
+    
+    {
+        type: "ruku",
+        id: 3,
+        title: "2. Лицемеры",
+        description: [
+            {
+                text: "Лицемеры и последствия лицемерия",
+                ref: "2:[8–16]"
+            },
+            {
+                text: "Притчи о них",
+                ref: "2:[17–20]"
+            }
+        ],
+        tags: [],
+        chapter: 2,
+        from: 8,
+        to: 20,
+        juz: 1,
+        boldSeparators: [16]
+    },
+    
+    {
+        type: "ruku",
+        id: 4,
+        title: "3. Обращение ﷲ к человечеству",
+        description: [
+            {
+                text: "Требование ﷲ поклоняться исключительно Ему",
+                ref: "2:[21–22]"
+            },
+            {
+                text: "Утверждение, что Коран — Писание ﷲ",
+                ref: "2:[23–24]"
+            },
+            {
+                text: "Награда для верующих",
+                ref: "2:[25]"
+            },
+            {
+                text: "Притча о комаре: одних сбивает, других просвещает",
+                ref: "2:[26–27]"
+            },
+            {
+                text: "Как вы можете отвергать ﷲ?",
+                ref: "2:[28–29]"
+            }
+        ],
+        tags: [],
+        chapter: 2,
+        from: 21,
+        to: 29,
+        juz: 1,
+        boldSeparators: [22, 24, 25, 27, 29]
+    },
+    
+    {
+        type: "ruku",
+        id: 5,
+        title: "4. Адам ﵇ — первый человек",
+        description: [
+            { text: "История создания Адама ﵇", ref: "2:[30]" },
+            { text: "Победа знания", ref: "2:[31–33]" },
+            { text: "Ангелы выражают уважение Адаму ﵇", ref: "2:[34–35]" },
+            { text: "Шайтан стал причиной лишения Рая", ref: "2:[36]" },
+            { text: "Покаяние Адама ﵇ и его прощение", ref: "2:[37]" }
+        ],
+        tags: [],
+        chapter: 2,
+        from: 30,
+        to: 37,
+        juz: 1,
+        boldSeparators: [30, 33, 35, 36, 37]
+    },
+
+    
+    { type: "ruku", id: 6, title: "5 [3:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 7, title: "6 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 8, title: "7 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 9, title: "8 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 10, title: "9 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 11, title: "10 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 12, title: "11 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 13, title: "12 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 14, title: "13 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 15, title: "14 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 16, title: "15 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 17, title: "16 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 18, title: "17 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 19, title: "18 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 20, title: "19 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 21, title: "20 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 22, title: "21 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 23, title: "22 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 24, title: "23 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 25, title: "24 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 26, title: "25 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 27, title: "26 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 28, title: "27 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 29, title: "28 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 30, title: "29 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 31, title: "30 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 32, title: "31 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 33, title: "32 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 34, title: "33 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 35, title: "34 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 36, title: "35 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 37, title: "36 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 38, title: "37 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 39, title: "38 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 40, title: "39 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+    { type: "ruku", id: 41, title: "40 [:]", tags: [], chapter: 2, from: 1, to: 7, juz: 1, boldSeparators: []  },
+];
